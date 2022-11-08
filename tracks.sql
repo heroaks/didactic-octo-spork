fd884abf99d231eca49f4daa21dcfc268ea3ebbc
Man07har <thakurmanoharkumar7@gmail.com>
2022-11-08 23:53:45 +0530

    Inital push

---
+++ tracks.sql
@@ -0,0 +1,18 @@
+CREATE TABLE TRACKS
+(
+TRACK_NO INT PRIMARY KEY,
+LENGTH VARCHAR(20)
+);
+
+insert into TRACKS
+values(12, "100 KMS");
+insert into TRACKS
+values(20, "560 KMS");
+insert into TRACKS
+values(10, "160 KMS");
+insert into TRACKS
+values(34, "1200 KMS");
+insert into TRACKS
+values(53, "791 KMS");
+insert into TRACKS
+values(61, "911 KMS");
---
+++ train.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE TRAIN
+(
+TID INT PRIMARY KEY,
+TNAME VARCHAR(20),
+TTYPE VARCHAR(20)
+);
+
+
+insert into TRAIN (
+TID,
+TNAME,
+TTYPE)
+values(1,"rajdhani","diesel train");
+insert into TRAIN (
+TID,
+TNAME,
+TTYPE)
+values(2,"Andaman Express","Passenger Train");
+insert into TRAIN (
+TID,
+TNAME,
+TTYPE)
+values(3,"Baghmati","Regional Train");
+insert into TRAIN (
+TID,
+TNAME,
+TTYPE)
+values(4,"Rani Chenamma Express","Locomotive Train");
+insert into TRAIN (
+TID,
+TNAME,
+TTYPE)
+values(5,"Chamundi Express","Short Distance Train");
